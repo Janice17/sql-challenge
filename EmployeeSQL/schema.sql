93517d69d60908170dd443df2281536b46f4cfb6
Janice17 <janiceguallpa@gmail.com>
2025-02-25 03:16:36 -0600

    Created a folder for EmployeeSQL work. Added the image of ERD, schema.sql, and queries.sql.

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,56 @@
+-- Drop tables if they already exist
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+
+-- Departments table
+CREATE TABLE departments (
+    dept_no VARCHAR(4)   NOT NULL PRIMARY KEY,
+    dept_name VARCHAR(35)   NOT NULL
+);
+
+-- Titles table
+CREATE TABLE titles (
+    title_id VARCHAR(5)   NOT NULL PRIMARY KEY,
+    title VARCHAR(30)   NOT NULL
+);
+
+-- Department employees table
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(5)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
+    hire_date DATE   NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Salaries table
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)	
+);
+
+-- Department employees table
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(4),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Department managers table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(4)   NOT NULL,
+    emp_no INT  NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
